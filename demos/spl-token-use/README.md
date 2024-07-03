e6f4c65ede8ec1b66a6015e3ddce16114cd52524
yqq <youngqqcn@gmail.com>
2024-07-03 11:30:03 +0800

    update

--- demos/spl-token-use/README.md
+++ demos/spl-token-use/README.md
@@ -3,3 +3,4 @@
 # 使用 `@solana/spl-token` 库
 
 
+参考： https://solanacookbook.com/references/token.html#how-to-create-a-new-token
--- demos/spl-token-use/index.mjs
+++ demos/spl-token-use/index.mjs
@@ -17,7 +17,8 @@ import {
 // import * as bs58 from "bs58";
 import bs58 from "bs58";
 
-(async () => {
+// 方式1： 使用内置的 createMint 函数 , 创建SPL Token
+async function createToken() {
     // connection
     const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
@@ -47,4 +48,49 @@ import bs58 from "bs58";
         8 // decimals
     );
     console.log(`mint: ${mintPubkey.toBase58()}`);
-})();
+}
+
+// 方式2： 自己组装交易指令
+async function createTokenV2() {
+    // connection
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+    let secretKey = bs58.decode(
+        "5TaF2mrj1wu6Kb7dj5AETQmg4Shaoo17wo3YucjTjZNP42wbR7sWN62f8tJpnNGEsJoxW8rQWWxiPH4qPxaxruCJ"
+    );
+
+    const feePayer = Keypair.fromSecretKey(secretKey);
+
+    // 打印base58格式的私钥
+    // console.log(bs58.encode(feePayer.secretKey));
+
+    const alice = new PublicKey("38jEaxphBTa3NEg4K6nG8Zgs6eVsSsr9AoSZCfax2pH8");
+
+    const mint = Keypair.generate();
+    console.log(`mint: ${mint.publicKey.toBase58()}`);
+    let tx = new Transaction().add(
+        // create mint account
+        SystemProgram.createAccount({
+            fromPubkey: feePayer.publicKey,
+            newAccountPubkey: mint.publicKey,
+            space: MINT_SIZE,
+            lamports: await getMinimumBalanceForRentExemptMint(connection),
+            programId: TOKEN_PROGRAM_ID,
+        }),
+        // init mint account
+        createInitializeMintInstruction(
+            mint.publicKey, // mint pubkey
+            8, // decimals
+            alice, // mint authority
+            alice // freeze authority (you can use `null` to disable it. when you disable it, you can't turn it on again)
+        )
+    );
+
+    // 第二个参数signers ， 需要传入签名的keypair
+    //           feePayer 是交易发起的签名
+    //           mint  是 Mint Account 的签名
+    console.log(`txhash is ${await connection.sendTransaction(tx, [feePayer, mint])}`)
+}
+
+// createToken();
+createTokenV2();
