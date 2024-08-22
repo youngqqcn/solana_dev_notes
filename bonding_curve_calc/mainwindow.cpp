862a2503a9d649c2a25311f59b927c90c623b3b0
yqq <youngqqcn@gmail.com>
2024-08-22 18:31:20 +0800

    增加计算买入的dy

--- bonding_curve_calc/mainwindow.cpp
+++ bonding_curve_calc/mainwindow.cpp
@@ -1,6 +1,16 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include <QPixmap>
+#include <QDebug>
+#include <QtNumeric>
+#include <QtMath>
+#include <QException>
+
+//const uint64_t DECIMALS = 1000000000;
+const uint64_t DECIMALS = 1;
+const uint64_t K = 1073000191 * DECIMALS;
+const uint64_t V = 32190000000 * DECIMALS;
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -8,6 +18,13 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     this->setWindowTitle("FanslandAI-BondingCurve-Calc");
+
+    qDebug() << "Minimum double value: "
+                  << std::numeric_limits<double>::min();
+    qDebug() << "Maximum double value: "
+                  << std::numeric_limits<double>::max() ;
+    qDebug() << "Smallest positive double value: "
+                  << std::numeric_limits<double>::denorm_min() ;
 
 
 
@@ -31,10 +48,70 @@ MainWindow::MainWindow(QWidget *parent)
     ui->label_formular_4->setPixmap(pm4);
     ui->label_formular_4->setScaledContents(true);
     ui->label_formular_4->resize(pm4.size());
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void MainWindow::on_le_now_pool_sol_amount_textChanged(const QString &input)
+{
+   calc_buy_common();
+}
+
+
+void MainWindow::on_le_buy_sol_amount_textChanged(const QString &arg1)
+{
+   calc_buy_common();
+}
+
+void MainWindow::calc_buy_common()
+{
+    if(0 == ui->le_buy_sol_amount->text().trimmed().length()){
+        return;
+    }
+    if(0 == ui->le_now_pool_sol_amount->text().trimmed().length()) {
+        return;
+    }
+
+    // convert to number
+    QString pool_sol_amount_str =  ui->le_now_pool_sol_amount->text();
+    qDebug() << "pool_sol_amount_str: " << pool_sol_amount_str;
+
+    double x = pool_sol_amount_str.toDouble();
+    qDebug() << "x: " << x;
+
+
+    QString buy_sol_amount_str =  ui->le_buy_sol_amount->text();
+    double dx = buy_sol_amount_str.toDouble();
+    qDebug() << "buy_sol_amount_str = " << buy_sol_amount_str;
+    qDebug() << "dx: " << dx;
+
+    double dy = calc_buy_for_dy(x, dx);
+
+    ui->le_will_get_token_amount->setText( QString::number(dy, 10, 6));
+}
+
+
+double MainWindow::calc_buy_for_dy(double x, double dx)
+{
+    qDebug() << "x = " << x;
+    qDebug() << "dx = " << dx;
+    double dy = (V * dx) / ((30 + x)*(30 + x + dx));
+    qDebug() << "dy = " << QString::number(dy, 'f', 6);
+    return dy;
+}
+
+uint64_t MainWindow::calc_sell_for_dx(uint64_t y, uint64_t dy)
+{
+    // TODO
+    return 0;
+}
+
+
+
+
 
--- bonding_curve_calc/mainwindow.h
+++ bonding_curve_calc/mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 
+
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -15,7 +17,21 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_le_now_pool_sol_amount_textChanged(const QString &arg1);
+
+    void on_le_buy_sol_amount_textChanged(const QString &arg1);
+
+private:
+    void calc_buy_common();
+    double calc_buy_for_dy(double x, double dx);
+    uint64_t calc_sell_for_dx(uint64_t y, uint64_t dy);
+
 private:
     Ui::MainWindow *ui;
+
+
+
+
 };
 #endif // MAINWINDOW_H
