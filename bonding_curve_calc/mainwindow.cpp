bcc88895e665de719685626db786cedb6543d014
yqq <youngqqcn@gmail.com>
2024-08-23 10:53:37 +0800

    update

--- bonding_curve_calc/mainwindow.cpp
+++ bonding_curve_calc/mainwindow.cpp
@@ -28,23 +28,23 @@ MainWindow::MainWindow(QWidget *parent)
 
 
 
-    QPixmap pm("/home/yqq/fansland/fansland_ai/bonding_curve_calc/formular_1.png"); // <- path to image file
+    QPixmap pm("./formular_1.png"); // <- path to image file
     ui->label_formular_1->setPixmap(pm);
     ui->label_formular_1->setScaledContents(true);
     ui->label_formular_1->resize(pm.size());
 
 
-    QPixmap pm2("/home/yqq/fansland/fansland_ai/bonding_curve_calc/formular_2.png"); // <- path to image file
+    QPixmap pm2("./formular_2.png"); // <- path to image file
     ui->label_formular_2->setPixmap(pm2);
     ui->label_formular_2->setScaledContents(true);
     ui->label_formular_2->resize(pm2.size());
 
-    QPixmap pm3("/home/yqq/fansland/fansland_ai/bonding_curve_calc/formular_3.png"); // <- path to image file
+    QPixmap pm3("./formular_3.png"); // <- path to image file
     ui->label_formular_3->setPixmap(pm3);
     ui->label_formular_3->setScaledContents(true);
     ui->label_formular_3->resize(pm3.size());
 
-    QPixmap pm4("/home/yqq/fansland/fansland_ai/bonding_curve_calc/formular_3.png"); // <- path to image file
+    QPixmap pm4("./formular_3.png"); // <- path to image file
     ui->label_formular_4->setPixmap(pm4);
     ui->label_formular_4->setScaledContents(true);
     ui->label_formular_4->resize(pm4.size());
@@ -83,6 +83,7 @@ void MainWindow::calc_buy_common()
     QString pool_sol_amount_str =  ui->le_buy_now_pool_sol_amount->text();
     qDebug() << "pool_sol_amount_str: " << pool_sol_amount_str;
 
+    bool ok = false;
     double x = pool_sol_amount_str.toDouble();
     qDebug() << "x: " << x;
 
