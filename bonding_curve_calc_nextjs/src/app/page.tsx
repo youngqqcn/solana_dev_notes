cfe5afa05ebbe21cad327b9b910dd39245eff71b
yqq <youngqqcn@gmail.com>
2024-08-26 11:09:54 +0800

    增加计算买入卖出代码

--- bonding_curve_calc_nextjs/src/app/page.tsx
+++ bonding_curve_calc_nextjs/src/app/page.tsx
@@ -21,9 +21,19 @@ export default function Home() {
     const [afterTradingSolAmountInPool, setAfterTradingSolAmountInPool] =
         useState("");
     const [tokenAmountInPool, setTokenAmountInPool] = useState("");
-    const [buyWillGetTokenAmount, setBuyWillGetTokenAmount] = useState("");
-    const [buySolAmount, setBuySolAmount] = useState("");
+
+    const [payInAmount, setPayInAmount] = useState("");
+    const [payOutAmount, setPayOutAmount] = useState("");
     const [marketPrice, setMarketPrice] = useState("");
+
+    // buy/sell 操作
+    const [selectedOption, setSelectedOption] = useState("buy");
+    const [isBuyOperation, setIsBuyOperation] = useState(true); // true: buy, false: sell
+
+    // 付出的Token文案
+    const [payOutText, setPayOutText] = useState("买入数量(SOL数量):");
+    const [payInText, setPayInText] = useState("得到的数量(Token数量):");
+    // const [calcButtonText, setCalcButtonText] = useState("计算买入");
 
     async function getTokenBalance(
         bondingCurvePDA: String,
@@ -42,11 +52,11 @@ export default function Home() {
         // let accInfo = await connection.getAccountInfo(new web3.PublicKey(bondingCurvePDA));
         // accInfo?.data.
 
-
-
         // TODO: 这里要减去账户租金, 最保险的做法，应该读取PDA账户中的变量值
         let accountSize = 130;
-        let rent = await connection.getMinimumBalanceForRentExemption(accountSize);
+        let rent = await connection.getMinimumBalanceForRentExemption(
+            accountSize
+        );
         console.log("rent =", rent);
 
         let rawSolBalance = await connection.getBalance(
@@ -55,8 +65,6 @@ export default function Home() {
         console.log("rawSolBalance = ", rawSolBalance);
         let trueSolBalance = rawSolBalance - rent; // 减去租金
         console.log("trueSolBalance = ", trueSolBalance);
-
-
 
         // 创建钱包地址的 PublicKey
         const walletPublicKey = new PublicKey(bondingCurvePDA);
@@ -126,35 +134,102 @@ export default function Home() {
             console.log("bondingCurvePDA = ", bondingCurvePDA);
             console.log("tokenMint = ", tokenMint);
             console.log("tokenAmountInPool = ", tokenAmountInPool);
-            console.log("buyWillGetTokenAmount = ", buyWillGetTokenAmount);
-            console.log("buySolAmount = ", buySolAmount);
             console.log("marketPrice = ", marketPrice);
+            console.log("payInAmount = ", payInAmount);
+            console.log("payOutAmount = ", payOutAmount);
 
             let rsp = await getTokenBalance(bondingCurvePDA, tokenMint);
             console.log("======获取结果: ");
             console.log(rsp);
 
             // 计算买入
-            let x = rsp.sol;
-            let dx = parseFloat(buySolAmount);
-            let dy = calc_buy_for_dy(x, dx);
+            let [dx, dy, mprice] = [0, 0, 0];
+            if (isBuyOperation) {
+                // 买入token
+                let x = rsp.sol;
+                dx = parseFloat(payOutAmount);
+                dy = calc_buy_for_dy(x, dx);
+                console.log("dy = ", dy);
 
-            // 计算最新成交价
-            let price = calc_market_price(x + dx);
+                // 计算最新成交价
+                mprice = calc_market_price(x + dx);
+                setPayInAmount(dy.toFixed(6).toString());
+                setMarketPrice(mprice.toFixed(10).toString());
+                setCurSolAmountInPool(rsp.sol.toFixed(9).toString());
+                setTokenAmountInPool(rsp.token.toFixed(6).toString());
+                setAfterTradingSolAmountInPool(
+                    (rsp.sol + dx).toFixed(9).toString()
+                );
+            } else {
+                // 卖出token
+                let x = rsp.sol;
+                let y = rsp.token;
+                dy = parseFloat(payOutAmount);
+                dx = calc_sell_for_dx(y, dy);
+                console.log("dx = ", dx);
 
-            setMarketPrice(price.toFixed(10).toString());
-            setBuyWillGetTokenAmount(dy.toFixed(6).toString());
-            setCurSolAmountInPool(rsp.sol.toFixed(9).toString());
-            setTokenAmountInPool(rsp.token.toFixed(6).toString());
-            setAfterTradingSolAmountInPool((x + dx).toFixed(9).toString());
+                // 计算最新成交价
+                mprice = calc_market_price(x - dx);
+                setPayInAmount(dx.toFixed(9).toString()); // sol
+                setMarketPrice(mprice.toFixed(10).toString());
+                setCurSolAmountInPool(rsp.sol.toFixed(9).toString());
+                setTokenAmountInPool(rsp.token.toFixed(6).toString());
+                setAfterTradingSolAmountInPool(
+                    (rsp.sol - dx).toFixed(9).toString()
+                );
+            }
         } catch (error) {
             console.error("Error occured:", error);
         }
     };
 
+    const handleOptionChange = (event: any) => {
+        let select = (() => {
+            switch (event.target.value) {
+                case "buy":
+                    return "buy";
+                case "sell":
+                    return "sell";
+                default:
+                    return "buy";
+            }
+        })();
+
+        setSelectedOption(select);
+        setPayOutText(
+            select == "buy" ? "买入数量(SOL数量):" : "卖出数量(Token数量):"
+        );
+        setPayInText(
+            select == "buy" ? "得到的数量(Token数量):" : "得到的数量(SOL数量):"
+        );
+
+        // 设置交易类型
+        setIsBuyOperation(select == "buy" ? true : false);
+    };
+
     return (
         <div>
-            <h2>买入计算</h2>
+            <h2>FanslandAI-交易计算</h2>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <label style={{ marginRight: "20px" }}>
+                    <input
+                        type="radio"
+                        value="buy"
+                        checked={selectedOption === "buy"}
+                        onChange={handleOptionChange}
+                    />
+                    买入Token
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        value="sell"
+                        checked={selectedOption === "sell"}
+                        onChange={handleOptionChange}
+                    />
+                    卖出Token
+                </label>
+            </div>
 
             <div>
                 <label>BondingCurve PDA地址 : </label>
@@ -176,22 +251,31 @@ export default function Home() {
                 />
             </div>
 
-            <div>
-                <label>付出SOL的数量</label>
-                <input
-                    type="text"
-                    value={buySolAmount}
-                    onChange={(e) => setBuySolAmount(e.target.value)}
-                />
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <div style={{ marginRight: "20px" }}>
+                    <label>{payOutText}</label>
+                    <input
+                        type="number"
+                        value={payOutAmount}
+                        onChange={(e) => setPayOutAmount(e.target.value)}
+                        placeholder="请输入数量......"
+                    />
+                </div>
+                <button onClick={handleCalculate}>开始计算</button>
             </div>
 
             <div>
-                <label>池中当前SOL数量</label>
+                <label>{payInText}</label>
+                <input type="text" value={payInAmount} disabled />
+            </div>
+
+            <div>
+                <label>当前池中当前SOL数量: </label>
                 <input type="text" value={curSolAmountInPool} disabled />
             </div>
 
             <div>
-                <label>交易后池子中SOL数量</label>
+                <label>交易后池中SOL数量: </label>
                 <input
                     type="text"
                     value={afterTradingSolAmountInPool}
@@ -200,34 +284,18 @@ export default function Home() {
             </div>
 
             <div>
-                <label>得到Token的数量</label>
-                <input type="text" value={buyWillGetTokenAmount} disabled />
+                <label>当前池中的Token数量: </label>
+                <input type="text" value={tokenAmountInPool} disabled />
             </div>
             <div>
-                <label>最新成交价(市场价)</label>
-                <input type="text" value={marketPrice} disabled />
-            </div>
-            <div>
-                <label>池中的Token数量</label>
+                <label>交易后池中的Token数量: </label>
                 <input type="text" value={tokenAmountInPool} disabled />
             </div>
 
-            {/* <h2>卖出计算</h2>
-
             <div>
-                <label>卖出Token的数量</label>
-                <input type="text" value={""} disabled />
-            </div>
-            <div>
-                <label>得到SOL的数量</label>
-                <input type="text" value={""} disabled />
-            </div>
-            <div>
-                <label>最新成交价(市场价)</label>
+                <label>最新成交价(市场价): </label>
                 <input type="text" value={marketPrice} disabled />
-            </div> */}
-
-            <button onClick={handleCalculate}>Calculate</button>
+            </div>
         </div>
     );
 }
